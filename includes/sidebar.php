1325c31eecdc69e46a6499af1676c54170af2108
Felplex01 <luisfelipesilietaveras@gmail.com>
2024-12-03 00:42:02 -0500

    -Layout creado

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,26 +1,39 @@
 <?php if (!isset($hideSidebar) || !$hideSidebar): ?>
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="../views/dashboard.php" class="brand-link">
-        <span class="brand-text font-weight-light">CEPVA</span>
-    </a>
-    <div class="sidebar">
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
-                <li class="nav-item">
-                    <a href="../views/student.php" class="nav-link">
-                        <i class="fas fa-graduation-cap nav-icon"></i>
-                        <p>Estudiantes</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="../views/attendance.php" class="nav-link">
-                        <i class="nav-icon fas fa-chalkboard-teacher"></i>
-                        <p>Asistencia</p>
-                    </a>
-                </li>
-                <!-- Agrega los demás enlaces según tus necesidades -->
-            </ul>
-        </nav>
-    </div>
-</aside>
+  <!-- Sidebar -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <a href="" class="brand-link">
+                <span class="brand-text font-weight-light">CEPVA</span>
+            </a>
+            <div class="sidebar">
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                                <p>Asistencia</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href=" views/VerEstudiantes.php" class="nav-link">
+                                <i class="nav-icon fas fa-graduation-cap"></i>
+                                <p>Estudiantes</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                <i class="nav-icon fas fa-users"></i>
+                                <p>Docente</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                <i class="nav-icon fas fa-user-shield"></i>
+                                <p>Administrador</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+
 <?php endif; ?>
--- views/EditarPerfil.php
+++ views/EditarPerfil.php
@@ -1,3 +1,7 @@
+<?php
+        include '../includes/header.php';
+        include '../includes/sidebar.php';
+        ?>
 <!DOCTYPE html>
 <html lang="es">
 

671f886f024162353cde1ed5daf3c4335064a786
ZetitaDev <EfrenDaniel2610@gmail.com>
2024-12-08 09:27:24 -0500

    .

--- views/EditarPerfil.php
+++ views/EditarPerfil.php
@@ -214,8 +214,6 @@ include '../includes/sidebar.php';
             </div>
         </div>
     </div>
-
-  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         function openAvatarModal() {

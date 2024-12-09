d9bd29eb463cc1b54feef759706df54da059e299
Felplex01 <luisfelipesilietaveras@gmail.com>
2024-12-09 08:10:47 -0500

    -Formularios arreglados

--- views/EditarPerfil.php
+++ views/EditarPerfil.php
@@ -40,6 +40,7 @@ include '../includes/sidebar.php';
                                 <div class="card-body">
                                     <div class="form-container">
                                         <!-- Column 1 -->
+                                         <form>
 
                                         <div class="form-column">
                                             <div class="form-group">
@@ -149,6 +150,7 @@ include '../includes/sidebar.php';
                                         
                                 </div>
                             </div>
+                            </form>
                         </section>
                     </div>
                 </div>
